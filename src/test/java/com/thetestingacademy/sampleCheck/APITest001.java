3e297928a847ed19b70c792fa3c69e8cb4b08133
Nayan Patil <nayanhdesai@gmail.com>
2025-03-23 23:17:20 +0530

    Added APITest002

---
+++ src/test/java/com/thetestingacademy/sampleCheck/APITest001.java
@@ -0,0 +1,8 @@
+package com.thetestingacademy.sampleCheck;
+
+
+public class APITest001 {
+    public static void main(String[] args) {
+        System.out.println("API Testing");
+    }
+}
---
+++ src/test/java/com/thetestingacademy/sampleCheck/APITest002.java
@@ -0,0 +1,21 @@
+package com.thetestingacademy.sampleCheck;
+
+import io.restassured.RestAssured;
+
+public class APITest002 {
+    public static void main(String[] args) {
+        //Gherkin Syntax
+        //Full URL->https://restful-booker.herokuapp.com/booking/1
+        //base URI->https://restful-booker.herokuapp.com
+        //base Path->/booking/1
+
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/booking/1")
+                .when()
+                    .get()
+                .then().log().all()
+                    .statusCode(200);
+    }
+}
